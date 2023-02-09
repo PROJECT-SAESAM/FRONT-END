026825baa99cbbb744992aaf2202eada9e8761db
410am <rlclqk@naver.com>
2023-02-09 20:51:25 +0900

    0209/ 우아아아아 checkbox 파트 코드정리 완료

--- src/Layout/Footer.tsx
+++ src/Layout/Footer.tsx
@@ -1,12 +1,66 @@
+import axios from 'axios';
+import { useState } from "react";
+import { ReactNode } from "react";
+import { json } from 'stream/consumers';
+
 
 const Footer = () => {
+
+    // // 키워드리스트 받아오는 axios를 통해서 페이지 들어오자 마자 요청 받아서 state에 저장
+    // const GetKeyword = async (userid) => {
+    //     await axios.get({url}, {
+    //         params: {
+    //             username: userid,
+    //         },
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             "Authorization": "Bearer " + sessionStorage.getItem('access_token')
+
+    //         }
+    //     }).then(res => {
+    //         const data = res.data // 이게 json형식으로 오는거
+    //         setPlaceType(data.firstType);
+    //         setMoodType(data.secondType);
+    //     }).catch(error => {
+    //         console.log(error, ' GetBasketItem 에러');
+    //     })
+    // }
+
+    // const [fruit, setFruit] = useState("");
+    // const url = "http://127.0.0.1:8000/fruits"
+
+    // const typeFruit = async () => {
+    //     console.log("되나");
+    //     await axios.get(url)
+    //         .then(function (response) {
+    //             console.log("인풋 성공");
+    //         }).then((response: any) => {
+    //             const data = response.data // 이게 json형식으로 오는거
+    //             setFruit(response.data[0]);
+    //         }).catch(error => {
+    //             console.log(error, ' GetBasketItem 에러');
+    //         })
+    //     return <div>{fruit}</div>
+    // }
+
+    // typeFruit()
+
+    // await jsonp(url, null, (err, res) => {
+
+    // });
+
+    // const getData = () = {
+    //     axios.get('http://127.0.0.1:8000/fruits').then((response:any) => console.log(response.data))
+    // }
     return (
         <div className='h-fit' >
             <div className="font-serif bg-green-700">
                 {/* 폰트 바꿔야됨 */}
+                {/* <div>{fruit}</div> */}
                 <p className=" flex justify-center p-16 pb-12 text-7xl font-thin text-[#eae7de]">
                     새삼스러울, 서울
                 </p>
+
                 <div className="flex place-content-between">
                     <div className="flex text-[#eae7de] pb-12 pt-10">
                         <div className="pl-12">
--- src/Layout/Main/RCMND/Check.tsx
+++ src/Layout/Main/RCMND/Check.tsx
@@ -1,5 +1,6 @@
 
-import { useState } from "react";
+import { log } from "console";
+import { ChangeEvent, ChangeEventHandler, useState } from "react";
 import { ReactNode } from "react";
 
 
@@ -13,29 +14,23 @@ const Check = () => {
 
     // 테스트용 카테고리 더미
     const firstCategory: CategoryType = { 'placeType': ['맛집', '카페', '술집'] };
-    const secondCategory: CategoryType = {
-        'station': ['성수역', '뚝섬역', '서울숲역'],
-        'distance': ['도보 5분 이내', '도보 5분~10분', '도보10분~15분', '도보 15분 이상']
-    };
-    const thirdCategory: CategoryType = {
-        'cafe': ['카공', '데이트', '대화', '모임'],
-        'restaurant': ['양식', '한식', '중식', '일식'],
-        'bar': ['막걸리', '맥주', '소주', '와인', '칵테일']
-    };
-
-    // 현재 카테고리
-    const [category, setCategory] = useState<CategoryType>(firstCategory);
-
-    // 몇번째 카테고리인지
-    // let th = 0;
-    // const [th, setTh] = useState<number>(0);
+    const secondCategoryOne: CategoryType = { 'station': ['성수역', '뚝섬역', '서울숲역'] };
+    const secondCategoryTwo: CategoryType = { 'distance': ['도보 5분 이내', '도보 5분~10분', '도보10분~15분', '도보 15분 이상'] };//(categorys[1])['distance']
+    const thirdCategory: CategoryType = { 'details': ['혼밥혼술', '맛있는', '신선한', '다양한', '가성비', '가심비', '인테리어', '이색적인', '작업공간', '데이트', '모임', '서비스', '편의성', '선물하기 좋은', '깨끗한', '주차가능', '반려동물동반', '가족끼리'] }
 
     // 체크된 keyword를 담을 리스트
     const [checkedKeywordList, setCheckedKeywordList] = useState<string[]>([]);
 
-    const cate: CategoryType[] = [firstCategory, secondCategory, thirdCategory];
-    console.log(cate[1]);
-    // const [thn, setThn] = useState<number>(0);
+    //최종 리스트 만들기
+    const [finalList, setFinalList] = useState<CategoryType>({});
+
+    //카테고리들 리스트
+    const categorys: CategoryType[] = [secondCategoryOne, secondCategoryTwo, thirdCategory];
+
+    // 현재 카테고리
+    let [th, setTh] = useState<number>(0);
+    const [category, setCategory] = useState<CategoryType>(firstCategory);
+
 
     // 체크박스 컴포넌트
     const keywordCheckBox = (category: CategoryType): ReactNode => {
@@ -45,21 +40,19 @@ const Check = () => {
         // 키워드 리스트(value)
         let values: string[] = category[key];
 
-        console.log({ key, values });
 
         // 키워드 리스트 돌려서 체크박스 생성 
         const checkBox =
             values.map((value) => {
                 return (//체크박스 디자인 바꾸기
                     <>
-                        <p className="p-2"><label><input type="checkbox" id={value} value={value} onChange={(e) => singleCheck(e.target.checked, e.target.value)}
+                        <p className="p-2"><label><input type="checkbox" id={value} value={value} name={key} onChange={(e) => singleCheck(e.target.checked, e.target.value)}
                             checked={checkedKeywordList.includes(value) ? true : false} />{value}</label>
                         </p>
                     </>
                 )
             })
 
-        console.log("맨 첨" + category)
         return (
             <>
                 <p className="w-full p-2 h-fit">
@@ -94,37 +87,115 @@ const Check = () => {
         }
     }
 
-    // 이전 버튼
-    const backButton = () => {
+    // 다음 버튼
+    const nextButton = (): ReactNode => {
+
+        // 다음 버튼 누르면
+        const nextClickHandler = (event: React.MouseEvent<HTMLElement>): void => {
+
+            //3번째 카테고리면 멈춰야돼서 이전까지만 돌려돌려
+            if (th < 3) {
+                setTh(th + 1)
+                setCategory(categorys[th])
+            }
+
+            //현 카테고리의 key(placeType, distance, detail)
+            const keyOfCategory = Object.keys(category)[0];
+
+            //최종리스트에 key(카테고리):value(키워드)로 넣기
+            setFinalList((finalList) => { return { ...finalList, [keyOfCategory]: checkedKeywordList } })
+            console.log(finalList)
+
+            //저장하고나면 빈 배열로 초기화
+            setCheckedKeywordList([]);
+        };
+
         return (
             <>
-                <p className="pr-8 text-sm font-medium text-gray-500 w-fit hover:-translate-y-1 hover:underline">
+                <p className="pl-8 text-sm font-medium text-gray-500 w-fit hover:-translate-y-1 hover:underline">
                     <label>
-                        <button type="button" name="backButton" className="translate-y-2">
+                        <span>Next</span>
+                        <button type="button" name="nextButton" className="translate-y-2" onClick={(e) => nextClickHandler(e)}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#39AE68" className="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
                             </svg>
                         </button>
-                        <span className="">Back</span>
                     </label>
                 </p>
             </>
         )
     }
 
-    const th = () => {
-        console.log("넥스트버튼 클릭됨")
-        if (category == firstCategory) {
-            setCategory(secondCategory)
-            console.log(category)
-            return keywordCheckBox(category)
-        } else if (category == secondCategory) {
-            setCategory(thirdCategory)
-            console.log(category)
-            return keywordCheckBox(category)
-        } else { console.log(category) }
+    //이전이랑 다음버튼 겹치는부분 함수로 빼기 - 미완료
+
+    // // 이전이나 다음 누르면
+    // const checkMoveHandler = (e: React.MouseEvent<HTMLButtonElement>): void => {
+    //     const target = e.target as Element;
+    //     console.log(th + "번째")
+    //     //3번째 카테고리면 멈춰야돼서 이전까지만 돌려돌려
+    //     if (th < 3) {
+    //         if (e.currentTarget.id === "next") {
+    //             setTh(th + 1)
+    //             console.log(th + "더함")
+    //         } else {
+    //             setTh(th - 1)
+    //             console.log(th + "뺌")
+    //             console.log("엥 이건ㄴ데" + e.currentTarget.id)
+    //         }
+
+    //         setCategory(categorys[th])
+    //     }
+
+    //     //현 카테고리의 key(placeType, distance, detail)
+    //     const keyOfCategory = Object.keys(category)[0];
+
+    //     //최종리스트에 key(카테고리):value(키워드)로 넣기
+    //     setFinalList((finalList) => { return { ...finalList, [keyOfCategory]: checkedKeywordList } })
+    //     console.log(finalList)
+
+    //     //저장하고나면 빈 배열로 초기화
+    //     setCheckedKeywordList([]);
+    // };
+
+    // 이전 버튼
+    const backButton = (): ReactNode => {
+
+        // 다음 버튼 누르면
+        const backClickHandler = (event: React.MouseEvent<HTMLElement>): void => {
+
+            //3번째 카테고리면 멈춰야돼서 이전까지만 돌려돌려
+            if (th < 3) {
+                setTh(th + 1)
+                setCategory(categorys[th])
+            }
+
+            //현 카테고리의 key(placeType, distance, detail)
+            const keyOfCategory = Object.keys(category)[0];
+
+            //최종리스트에 key(카테고리):value(키워드)로 넣기
+            setFinalList((finalList) => { return { ...finalList, [keyOfCategory]: checkedKeywordList } })
+            console.log(finalList)
+
+            //저장하고나면 빈 배열로 초기화
+            setCheckedKeywordList([]);
+        };
+
+
+        return (
+            <>
+                <p className="pr-8 text-sm font-medium text-gray-500 w-fit hover:-translate-y-1 hover:underline">
+                    <label>
+                        <button type="button" id="back" name="backButton" className="translate-y-2" onClick={(e) => backClickHandler(e)}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#39AE68" className="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+                            </svg>
+                        </button>
+                        <span>Back</span>
+                    </label>
+                </p>
+            </>
+        )
     }
-    console.log(category)
 
 
 
@@ -139,19 +210,8 @@ const Check = () => {
                 <div className="flex flex-wrap pt-3 -translate-y-8 h-fit">
                     <div className="flex flex-row flex-wrap content-start justify-start gap-4 p-3 h-96">{keywordCheckBox(category)}</div>
                     <div className="flex flex-row justify-between w-full pt-10 pb-1 pr-2">
-                        {/* <div>{backButton()}</div> */}
-                        <>
-                            <p className="pl-8 text-sm font-medium text-gray-500 w-fit hover:-translate-y-1 hover:underline">
-                                <label>
-                                    <span>Next</span>
-                                    <button type="button" name="nextButton" className="translate-y-2" onClick={() => (th())} >
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#39AE68" className="w-6 h-6">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-                                        </svg>
-                                    </button>
-                                </label>
-                            </p>
-                        </>
+                        <div>{backButton()}</div>
+                        <div>{nextButton()}</div>
                     </div>
                 </div>
             </div>
